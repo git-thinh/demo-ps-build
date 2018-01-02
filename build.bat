963799512b0768a4e78d4738647509a2ab1043d2
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-02 13:14:39 +0700

    UPDATE

--- build.bat
+++ build.bat
@@ -11,28 +11,36 @@ ECHO PATH_ROOT = %PATH_ROOT%
 ECHO CODE = %CODE%
 ECHO BUILD_CODE = %BUILD_CODE%
 
+CALL :BLANK_LINE
 ::IF "%CODE%"=="001" GOTO BUILD_001
 GOTO %BUILD_CODE%
 ::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:BUILD_001
-    ECHO.  
-    :: Clean target  
-    DEL /s /q /f *.o
-    DEL /s /q /f *.a
-    DEL /s /q /f *.dll
-    DEL /s /q /f *.exe
+:BUILD_001    
+    CALL :CLEAN_TARGET
 
     :: Compile the source files into object files
     gcc -c main.cpp -o main.o
-
     :: Link the object files into a binary
     gcc -o app.exe main.o -lstdc++
+    
+    CALL :BLANK_LINE
+    CALL app.exe
 
-    call app.exe
     GOTO :BUILD_END
 ::::::::::::::::::::::::::::::::::::::::::::::::::::
+:CLEAN_TARGET
+    DEL /s /q /f *.o
+    DEL /s /q /f *.a
+    DEL /s /q /f *.dll
+    DEL /s /q /f *.exe    
+EXIT /B 0
+
+:BLANK_LINE 
+    ECHO.
+    ECHO.
+    ECHO.
+EXIT /B 0
+
 :BUILD_END
-    ECHO.
-    ECHO.
-    ECHO.
+    CALL :BLANK_LINE
